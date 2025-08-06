86e0ce5cc4aa5babad114fcd26e8a5d24a9b19db
Sufyan <fake.email@domain.com>
2025-08-06 16:07:10 +0530

    Problem 1

---
+++ problem-0/01.js
@@ -0,0 +1,13 @@
+/* 
+Write a JS program to print all natural numbers 
+from 1 to n. – using while loop 
+*/
+function printAllNaturalNumber(num){
+    let i = 1;
+    while(i<=num) {
+        console.log(i);
+        i++;
+
+    }
+}
+printAllNaturalNumber(100)
