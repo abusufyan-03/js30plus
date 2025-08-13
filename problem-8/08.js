590a0e48e316074b3390f3c4fa42255fb2f8b47d
Sufyan <fake.email@domain.com>
2025-08-13 15:51:37 +0530

    Remove old problem folders

--- problem-8/08.js
+++
@@ -1,9 +0,0 @@
-/*  
-08. Write a C program to print multiplication table of any number. 
-*/
-function multiplicationTable(num){
-    for(let i = 1; i<=10; i++){
-        console.log(`${num} * ${i} = ${num*i}`)
-    }
-}
-multiplicationTable(2)
