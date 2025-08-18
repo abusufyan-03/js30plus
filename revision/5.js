1b3b5e53dc894c61d135105a1c49035d1c8b2238
Sufyan <sufiyan.manuu@gmail.com>
2025-08-18 21:34:33 +0530

    removed revision

--- revision/5.js
+++
@@ -1,9 +0,0 @@
-// 6. Write a C program to find sum of all natural numbers between 1 to n
-function sumOfAllNatural(num){
-    let sum = 0;
-    for(let i = 1; i<=num; i++) {
-        sum+=i;
-    }
-    console.log(sum)
-}
-sumOfAllNatural(4)
