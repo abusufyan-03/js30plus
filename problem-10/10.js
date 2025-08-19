e4be7d08639bc54de4a97800fa74891ea4bff0d0
Sufyan <sufiyan.manuu@gmail.com>
2025-08-19 16:12:09 +0530

    Fixed while condition

---
+++ problem-10/10.js
@@ -0,0 +1,15 @@
+/* 
+10.  Write a C program to find first and last 
+digit of a number.
+*/
+function findFirstAndLast(num){
+    num = Math.abs(num);
+    let first, last;
+    last = num % 10;
+    while(num>=10){
+        num = Math.floor(num / 10);
+    }
+    first = num;
+    console.log(first, last)
+}
+findFirstAndLast(10)
